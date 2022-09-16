ec081fe38cb38a6c968c3ea253c3aa68daf657fd
Sondre Halvorsen <sondre.halvorsen@nav.no>
2022-09-16 15:23:49 +0200

    Bruk dataformat med teamnavn

--- src/main/kotlin/no/nav/helse/TeamDataFetcher.kt
+++ src/main/kotlin/no/nav/helse/TeamDataFetcher.kt
@@ -3,12 +3,12 @@ package no.nav.helse
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import java.io.File
 
-fun teamDataFromFile(): List<List<String>> {
+fun teamDataFromFile(): List<Pair<String, List<String>>> {
     // Hvis appen ikke kjører på NAIS så bruker den test data
     val teamJson = if (!System.getenv("NAIS_CLUSTER_NAME").isNullOrEmpty())
         File("/var/run/configmaps/team-data.json").readText() else
         object {}.javaClass.getResource("/test-team-data.json").readText()
 
     val teamNode = jacksonObjectMapper().readTree(teamJson)
-    return teamNode.map { team -> team.asSequence().map { it.asText() }.toList() }
+    return teamNode.map { team -> team["name"].asText() to team["members"].asSequence().map { it.asText() }.toList() }
 }
