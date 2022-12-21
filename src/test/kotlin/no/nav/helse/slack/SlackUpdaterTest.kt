b6d7621477a9e306795a92a767fd18153385417a
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2022-12-21 20:29:01 +0100

    Trenger ikke mock for clock, si ⏱️

--- src/test/kotlin/no/nav/helse/slack/SlackUpdaterTest.kt
+++ src/test/kotlin/no/nav/helse/slack/SlackUpdaterTest.kt
@@ -1,11 +1,11 @@
 package no.nav.helse.slack
 
-import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
 import no.nav.helse.AbstractRedTeamTest
 import org.junit.jupiter.api.Test
 import java.time.*
+import java.time.temporal.ChronoField
 
 internal class SlackUpdaterTest: AbstractRedTeamTest() {
 
@@ -37,36 +37,34 @@ internal class SlackUpdaterTest: AbstractRedTeamTest() {
     @Test
     fun `post and update next day (testing the lock)`() {
         val slackClient = mockk<RedTeamSlack>(relaxUnitFun = true)
-        val clock = mockk<Clock>()
-        every { clock.instant() } returns startDateAt(8).instant()
-        every { clock.zone } returns  startDateAt(8).zone
-        val updater = SlackUpdater({ LocalDateTime.now(clock) }, slackClient, redTeam())
+        var testklokke = testklokke(8)
+        val updater = SlackUpdater({ LocalDateTime.now(testklokke) }, slackClient, redTeam())
 
         updater.update()
 
         verify(exactly = 1) { slackClient.updateRedTeamGroup(any()) }
         verify(exactly = 1) { slackClient.postRedTeam(any()) }
 
-        every { clock.instant() } returns startDateAt(9).instant()
-        every { clock.zone } returns  startDateAt(9).zone
+        testklokke = testklokke(9)
 
         updater.update()
 
         verify(exactly = 1) { slackClient.updateRedTeamGroup(any()) }
         verify(exactly = 1) { slackClient.postRedTeam(any()) }
 
-        every { clock.instant() } returns startDateAt(8).instant()
-        every { clock.zone } returns  startDateAt(8).zone
+        testklokke = testklokke(8)
 
         updater.update()
 
         verify(exactly = 2) { slackClient.updateRedTeamGroup(any()) }
         verify(exactly = 2) { slackClient.postRedTeam(any()) }
-
     }
 
-    private fun startDateAt(hour: Int): Clock {
+    private fun testklokke(hour: Int): Clock {
         val offset = ZoneOffset.ofHours(1)
-        return Clock.fixed(START_DATE.atTime(hour, 0).toInstant(offset), ZoneId.ofOffset("", offset))
+        return Clock.fixed(
+            LocalDateTime.now().with(ChronoField.HOUR_OF_DAY, hour.toLong()).toInstant(offset),
+            ZoneId.ofOffset("", offset)
+        )
     }
 }
